926c5158af18ce0b70f64af650568e6736590cef
Soner Sayakci <github@shyim.de>
2022-10-07 11:38:39 +0000

    simplify command

--- src/Command/ExportCommand.php
+++ src/Command/ExportCommand.php
@@ -4,173 +4,70 @@ declare(strict_types=1);
 
 namespace Frosh\TemplateMail\Command;
 
+use Doctrine\DBAL\Connection;
+use Symfony\Component\Console\Command\Command;
 use Symfony\Component\Console\Input\InputArgument;
 use Symfony\Component\Console\Input\InputInterface;
-use Symfony\Component\Console\Input\InputOption;
 use Symfony\Component\Console\Output\OutputInterface;
+use Symfony\Component\Filesystem\Filesystem;
 
-/**
- * Export mail template from database to file system
- */
-class ExportCommand extends AbstractCommand
+class ExportCommand extends Command
 {
-    /**
-     * Database connection
-     *
-     * @var \Doctrine\DBAL\Connection
-     */
-    protected $databaseConnection;
+    protected static $defaultName = 'frosh:template-mail:export';
+    protected static $defaultDescription = 'Export mail templates from database to file system';
 
-    /**
-     * Root directory
-     *
-     * @var string
-     */
-    protected $directory;
+    private Connection $connection;
 
-    /**
-     * Filesystem
-     *
-     * @var \League\Flysystem\Filesystem
-     */
-    protected $filesystem;
-
-    /**
-     * Set repository for mail templates.
-     *
-     * @param \Doctrine\DBAL\Connection $databaseConnection
-     */
-    public function setDatabaseConnection(\Doctrine\DBAL\Connection $databaseConnection): void
+    public function __construct(Connection $connection)
     {
-        $this->databaseConnection = $databaseConnection;
+        parent::__construct();
+        $this->connection = $connection;
     }
 
-    /**
-     * @inerhitDoc
-     */
     protected function configure()
     {
         $this
-            ->setName('frosh:template-mail:export')
-            ->setDescription('Export mail templates from database to file system')
-            ->addArgument('directory', InputArgument::REQUIRED, 'Target directory')
-            ->addOption('dry-run', null, InputOption::VALUE_NONE, 'Dry run (only show what is being done)');
+            ->addArgument('directory', InputArgument::REQUIRED, 'Target directory');
     }
 
-    /**
-     * @inerhitDoc
-     */
-    public function execute(InputInterface $input, OutputInterface $output)
+    public function execute(InputInterface $input, OutputInterface $output): int
     {
-        // Print title.
-        $this->io->title(sprintf('%s (%s)', $this->getDescription(), $this->getName()));
+        $directory = $input->getArgument('directory');
 
-        // Get dry-run flag.
-        $isDryRun = (bool)$input->getOption('dry-run');
-
-        // Handle directory.
-        $this->directory = $input->getArgument('directory');
-        if (!$isDryRun && !$this->initializeFilesystem()) {
-            return 1;
-        }
-
-        // Fetch mail templates.
-        $query = <<<EOT
-SELECT mail_template_translation.*, mail_template_type.technical_name, locale.code AS locale
+        $query = <<<'SQL'
+SELECT 
+    mail_template_translation.content_plain, 
+    mail_template_translation.content_html, 
+    mail_template_translation.subject, 
+    mail_template_type.technical_name, 
+    locale.code AS locale
 FROM mail_template_translation
-JOIN mail_template ON mail_template.id = mail_template_translation.mail_template_id
-JOIN mail_template_type ON mail_template_type.id = mail_template.mail_template_type_id
-JOIN language ON language.id = mail_template_translation.language_id
-JOIN locale ON locale.id = language.locale_id
-EOT;
-        $records = $this->databaseConnection->fetchAllAssociative($query);
+INNER JOIN mail_template ON mail_template.id = mail_template_translation.mail_template_id
+INNER JOIN mail_template_type ON mail_template_type.id = mail_template.mail_template_type_id
+INNER JOIN `language` ON `language`.id = mail_template_translation.language_id
+INNER JOIN locale ON locale.id = language.locale_id
+SQL;
+        $records = $this->connection->fetchAllAssociative($query);
 
-        // Write mail templates to disk.
+        $fs = new Filesystem();
+
         foreach ($records as $record) {
-            if (!$this->writeMailTemplate($record, $isDryRun)) {
-                return 1;
+            $templateDir = sprintf('%s/%s/%s', $directory, $record['locale'], $record['technical_name']);
+
+            $map = [
+                'content_plain' => 'plain',
+                'content_html' => 'html',
+                'subject' => 'subject',
+            ];
+            foreach ($map as $field => $name) {
+                $targetFile = sprintf('%s/%s.twig', $templateDir, $name);
+
+                $fs->dumpFile($targetFile, $record[$field]);
             }
         }
 
-        return 0;
-    }
+        $output->writeln('Files has been exported');
 
-    /**
-     * Initialize filesystem.
-     *
-     * @return bool
-     */
-    protected function initializeFilesystem(): bool
-    {
-        // Make sure directory exists.
-        if (!is_dir($this->directory)) {
-            if (!@mkdir($this->directory, 0755, true) && !is_dir($this->directory)) {
-                $this->io->error(sprintf('Directory "%s" was not created', $this->directory));
-
-                return false;
-            }
-        }
-
-        // Initialize filesystem.
-        $this->filesystem = new \League\Flysystem\Filesystem(new \League\Flysystem\Adapter\Local($this->directory));
-        if (!$this->filesystem) {
-            $this->io->error('Filesystem could not be initialized');
-
-            return false;
-        }
-
-        return true;
-    }
-
-    /**
-     * Write mail template to disk.
-     *
-     * @param array $record
-     * @param bool $isDryRun
-     * @return bool
-     */
-    protected function writeMailTemplate(array $record, bool $isDryRun): bool
-    {
-        // Create directory.
-        $targetDirectory = sprintf('%s/%s', $record['locale'], $record['technical_name']);
-        if (!$isDryRun && !$this->filesystem->createDir($targetDirectory)) {
-            $this->io->error(sprintf('Directory "%s/%s" was not created', $this->directory, $targetDirectory));
-
-            return false;
-        }
-
-        // Write template parts.
-        $map = [
-            'content_plain' => 'plain',
-            'content_html' => 'html',
-            'subject' => 'subject',
-        ];
-        foreach ($map as $field => $name) {
-            $targetFile = sprintf('%s/%s.twig', $targetDirectory, $name);
-
-            $this->io->writeln(
-                sprintf(
-                    '<info>Writing part "%s" of template "%s" to file "%s/%s"</info>',
-                    $name,
-                    $record['technical_name'],
-                    $this->directory,
-                    $targetFile
-                )
-            );
-
-            if ($isDryRun) {
-                continue;
-            }
-
-            if (!$this->filesystem->put($targetFile, $record[$field])) {
-                $this->io->error(
-                    sprintf('Part "%s" of template "%s" was not exported', $name, $record['technical_name'])
-                );
-
-                return false;
-            }
-        }
-
-        return true;
+        return self::SUCCESS;
     }
 }
