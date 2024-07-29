7f647acb87f921cdbf91651384e4b2ae5414f350
Melvin Achterhuis <melvin.achterhuis@iodigital.com>
2024-07-29 22:32:19 +0200

    Fix convert error and use saleschannel name translation

--- src/Subscriber/FlowSubscriber.php
+++ src/Subscriber/FlowSubscriber.php
@@ -83,13 +83,19 @@ class FlowSubscriber implements EventSubscriberInterface
         }
 
         if ($subject) {
-            $salesChannelId = $dataBag->getString('salesChannelId');
+            $salesChannelId = $dataBag->get('salesChannelId');
             $debugMode = $this->systemConfigService->getBool('FroshPlatformTemplateMail.config.debugMode', $salesChannelId);
+
+            $salesChannelName = 'Administration';
+            if ($salesChannelId) {
+                $salesChannelName = $this->getSalesChannelName($salesChannelId, $context);
+            }
+
             if ($debugMode) {
                 $subject = sprintf(
                     'DEBUG: %s (%s - %s - %s)',
                     $subject,
-                    $this->getSalesChannelName($salesChannelId, $context),
+                    $salesChannelName,
                     $this->getLocaleCode($context->getLanguageId(), $context),
                     $technicalName,
                 );
@@ -149,6 +155,6 @@ class FlowSubscriber implements EventSubscriberInterface
         /** @var SalesChannelEntity $salesChannel */
         $salesChannel = $this->salesChannelRepository->search($criteria, $context)->first();
 
-        return $salesChannel->getName();
+        return $salesChannel->getTranslation('name');
     }
 }
