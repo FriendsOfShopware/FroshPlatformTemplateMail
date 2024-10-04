96a6d4c21bbe0ed96435c6137aab8e80c313fa4e
Alexander Menk <a.menk@imi.de>
2024-10-04 16:54:41 +0200

    Add option to export command to create UUID based structure
    #62

--- src/Command/ExportCommand.php
+++ src/Command/ExportCommand.php
@@ -5,10 +5,12 @@ declare(strict_types=1);
 namespace Frosh\TemplateMail\Command;
 
 use Doctrine\DBAL\Connection;
+use Shopware\Core\Framework\Uuid\Uuid;
 use Symfony\Component\Console\Attribute\AsCommand;
 use Symfony\Component\Console\Command\Command;
 use Symfony\Component\Console\Input\InputArgument;
 use Symfony\Component\Console\Input\InputInterface;
+use Symfony\Component\Console\Input\InputOption;
 use Symfony\Component\Console\Output\OutputInterface;
 use Symfony\Component\Filesystem\Filesystem;
 
@@ -33,6 +35,7 @@ SELECT
     mail_template_translation.content_html, 
     mail_template_translation.subject, 
     mail_template_type.technical_name, 
+    mail_template.id,
     locale.code AS locale
 FROM mail_template_translation
 INNER JOIN mail_template ON mail_template.id = mail_template_translation.mail_template_id
@@ -54,7 +57,11 @@ SQL;
                 'subject' => 'subject',
             ];
             foreach ($map as $field => $name) {
-                $targetFile = sprintf('%s/%s.twig', $templateDir, $name);
+                if ($input->getOption('uuid')) {
+                    $targetFile = sprintf('%s/%s/%s.twig', $templateDir, Uuid::fromBytesToHex($record['id']), $name);
+                } else {
+                    $targetFile = sprintf('%s/%s.twig', $templateDir, $name);
+                }
 
                 $fs->dumpFile($targetFile, $record[$field]);
             }
@@ -68,6 +75,7 @@ SQL;
     protected function configure(): void
     {
         $this
-            ->addArgument('directory', InputArgument::REQUIRED, 'Target directory');
+            ->addArgument('directory', InputArgument::REQUIRED, 'Target directory')
+            ->addOption('uuid', 'u', InputOption::VALUE_NONE, 'Create subfolders per UUID, to allow for multiple templates of the same type');
     }
 }
--- tests/Command/ExportCommandTest.php
+++ tests/Command/ExportCommandTest.php
@@ -7,6 +7,7 @@ namespace Frosh\TemplateMail\Tests\Command;
 use Doctrine\DBAL\Connection;
 use Frosh\TemplateMail\Command\ExportCommand;
 use PHPUnit\Framework\TestCase;
+use Shopware\Core\Framework\Uuid\Uuid;
 use Symfony\Component\Console\Tester\CommandTester;
 
 class ExportCommandTest extends TestCase
@@ -20,6 +21,7 @@ class ExportCommandTest extends TestCase
                 'content_html' => '<h1>My content</h1>',
                 'subject' => 'Test template',
                 'technical_name' => 'test_template',
+                'id' => Uuid::fromHexToBytes('0eb261c6de95464296df0c8fad57e5ab'),
                 'locale' => 'en',
             ],
         ]);
@@ -32,6 +34,41 @@ class ExportCommandTest extends TestCase
         $htmlFile = sys_get_temp_dir() . '/en/test_template/html.twig';
         $plainFile = sys_get_temp_dir() . '/en/test_template/plain.twig';
         $subjectFile = sys_get_temp_dir() . '/en/test_template/subject.twig';
+
+        static::assertFileExists($htmlFile);
+        static::assertFileExists($plainFile);
+        static::assertFileExists($subjectFile);
+
+        static::assertEquals('<h1>My content</h1>', file_get_contents($htmlFile));
+        static::assertEquals('My content', file_get_contents($plainFile));
+        static::assertEquals('Test template', file_get_contents($subjectFile));
+
+        static::assertSame(0, $tester->getStatusCode());
+        static::assertStringContainsString('Files has been exported', $tester->getDisplay());
+    }
+
+    public function testExportWithUuid(): void
+    {
+        $connection = $this->createMock(Connection::class);
+        $connection->method('fetchAllAssociative')->willReturn([
+            [
+                'content_plain' => 'My content',
+                'content_html' => '<h1>My content</h1>',
+                'subject' => 'Test template',
+                'technical_name' => 'test_template',
+                'id' => Uuid::fromHexToBytes('0eb261c6de95464296df0c8fad57e5ab'),
+                'locale' => 'en',
+            ],
+        ]);
+
+        $command = new ExportCommand($connection);
+
+        $tester = new CommandTester($command);
+        $tester->execute(['directory' => sys_get_temp_dir()], ['uuid' => true]);
+
+        $htmlFile = sys_get_temp_dir() . '/en/test_template/0eb261c6de95464296df0c8fad57e5ab/html.twig';
+        $plainFile = sys_get_temp_dir() . '/en/test_template/0eb261c6de95464296df0c8fad57e5ab/plain.twig';
+        $subjectFile = sys_get_temp_dir() . '/en/test_template/0eb261c6de95464296df0c8fad57e5ab/subject.twig';
 
         static::assertFileExists($htmlFile);
         static::assertFileExists($plainFile);
